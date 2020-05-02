8a50cb94811a9646e423f7ed435b67cc0086e664
Awa Dieudonne <dieudonneawa7@gmail.com>
2020-05-02 18:00:03 +0100

    create express server

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
-const app = async () => '#BuildforSDG';
+import express from 'express';
+import bodyParser from 'body-parser';
 
-export default app;
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  return console.log(`Server running on http://localhost:${port}`);
+});
