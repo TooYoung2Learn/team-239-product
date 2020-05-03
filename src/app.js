2059cb7b22be00e57cfe090dc374830cf5b4d866
Awa Dieudonne <dieudonneawa7@gmail.com>
2020-05-03 20:28:30 +0100

    create sign up & signin features

--- src/app.js
+++ src/app.js
@@ -1,20 +1,34 @@
-/* eslint-disable no-console */
+/* eslint-disable no-console, no-unused-vars */
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
+import cors from 'cors';
+import { handleError } from './helpers/error';
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
 
-app.get('/', (req, res) => res.status(200).send({ message: 'Welcome to our API' }));
+// Our API endpoints
+routes(app);
+
+// Error handler middleware
+app.use((err, req, res, next) => {
+  if (err) {
+    handleError(err, res);
+  }
+});
 
 const port = process.env.PORT || 4000;
 
 app.listen(port, (err) => {
-  if (err) {
+  if (err && err.status >= 400) {
     return console.log(err);
   }
   return console.log(`Server running on http://localhost:${port}`);
 });
+module.exports = app;
---
+++ src/middlewares/authentication.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-useless-escape */
+import validator from 'validator';
+import models from '../models';
+import { ErrorHandler } from '../helpers/error';
+
+export const validateSignUp = (req, res, next) => {
+  const { name, email, password } = req.body;
+  if (!name) {
+    throw new ErrorHandler(400, 'Name cannot be empty');
+  }
+  if (!validator.isEmail(email)) {
+    throw new ErrorHandler(400, 'Invalid email address');
+  }
+  if (password.length < 8) {
+    throw new ErrorHandler(400, 'Password length must be at least 8');
+  }
+  return next();
+};
+
+export const validateSignIn = (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!validator.isEmail(email) || !password) {
+    throw new ErrorHandler(400, 'Wrong email/password combination');
+  }
+  return next();
+};
